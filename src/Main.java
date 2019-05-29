d0762c5c0a3636750722d785af97cc88f816559f
Segovia <35817798+SegoCode@users.noreply.github.com>
2019-05-29 16:24:12 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+         TwichBot bot = new TwichBot("oauth:phljdm2znwydgasv4a5fngkjda6kj9"); //Fake code
+                                                                              // Generate your code here;
+                                                                              // https://twitchapps.com/tmi/
+    }
+    
+}
---
+++ src/TwichBot.java
@@ -0,0 +1,44 @@
+
+import java.io.IOException;
+import org.jibble.pircbot.*;
+
+public class TwichBot extends PircBot {
+
+    String oauthCode;
+    String channel = "ninja";
+
+    public TwichBot(String oauthCode) {
+        connectTwich(oauthCode);
+        this.setVerbose(true);
+
+    }
+
+    public void connectTwich(String oauthCode) {
+        try {
+            this.connect("irc.chat.twitch.tv", 6667, oauthCode);
+        } catch (NickAlreadyInUseException e) {
+            System.err.println("The bot is already in use");
+        } catch (IrcException e) {
+            System.err.println("Server Lost");
+        } catch (IOException ex) {
+            System.err.println("IO ERROR");
+        }
+
+    }
+
+    @Override
+    protected void onConnect() {
+        System.out.println("Connected!");
+        joinChannel("#" + channel);
+        super.onConnect();
+    }
+
+    @Override
+    public void onMessage(String channelTag, String Nick, String c, String d, String message) {
+        if(message.equals("!ping")){
+            sendMessage(channelTag, "pong");
+        }
+        
+    }
+
+}
